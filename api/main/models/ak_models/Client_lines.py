4b96f29a4efa8644f9d6fdb712c4b4180cac538f
mike-bionic <muhammedjepbarov@gmail.com>
2021-12-22 19:51:06 +0500

    added lines required by app

---
+++ api/main/models/ak_models/Client_lines.py
@@ -0,0 +1,83 @@
+# CREATE TABLE doner_test.dbo.tbl_mg_clientlines (
+# 	cl_id int IDENTITY(1,1) NOT NULL,
+# 	cl_datetime datetime DEFAULT getdate() NULL,
+# 	cl_total decimal(18,5) NULL,
+# 	cl_type int NULL,
+# 	cl_trans_name nvarchar(100) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+# 	inv_id int NULL,
+# 	bank_fich_head_id int NULL,
+# 	arap_id int NULL,
+# 	ks_line_id int NULL,
+# 	cl_credit decimal(18,5) NULL,
+# 	arap_id_acc_card_id int NULL,
+# 	op_bl_l_id int NULL,
+# 	inv_real_code nvarchar(50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+# 	spe_code nvarchar(50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+# 	group_code nvarchar(50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+# 	security_code nvarchar(50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+# 	cl_desc nvarchar(200) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+# 	p_id int NULL,
+# 	rep_rate decimal(18,5) DEFAULT 0 NULL,
+# 	rep_debit decimal(18,5) DEFAULT 0 NULL,
+# 	rep_credit decimal(18,5) DEFAULT 0 NULL,
+# 	cl_id_guid nvarchar(50) COLLATE SQL_Latin1_General_CP1_CI_AS DEFAULT newid() NULL,
+# 	CONSTRAINT PK_tbl_mg_clientlines PRIMARY KEY (cl_id)
+# );
+
+from datetime import datetime
+from sqlalchemy.dialects.postgresql import UUID
+
+from main import db
+
+class Mat_trans_line(db.Model):
+	__tablename__ = "tbl_mg_mat_trans_line"
+	cl_id = db.Column("cl_id",db.Integer,nullable=False,primary_key=True)
+	cl_datetime = db.Column("cl_datetime",db.DateTime,default=datetime.now())
+	cl_total = db.Column("cl_total",db.Integer)
+	cl_type = db.Column("cl_type",db.Integer)
+	cl_trans_name = db.Column("cl_trans_name",db.Integer)
+	inv_id = db.Column("inv_id",db.Integer)
+	bank_fich_head_id = db.Column("bank_fich_head_id",db.Integer)
+	arap_id = db.Column("arap_id",db.Integer)
+	ks_line_id = db.Column("ks_line_id",db.Integer)
+	cl_credit = db.Column("cl_credit",db.Integer)
+	arap_id_acc_card_id = db.Column("arap_id_acc_card_id",db.Integer)
+	op_bl_l_id = db.Column("op_bl_l_id",db.Integer)
+	inv_real_code = db.Column("inv_real_code",db.Integer)
+	spe_code = db.Column("spe_code",db.Integer)
+	group_code = db.Column("group_code",db.Integer)
+	security_code = db.Column("security_code",db.Integer)
+	cl_desc = db.Column("cl_desc",db.Integer)
+	p_id = db.Column("p_id",db.Integer)
+	rep_rate = db.Column("rep_rate",db.Integer)
+	rep_debit = db.Column("rep_debit",db.Integer)
+	rep_credit = db.Column("rep_credit",db.Integer)
+	cl_id_guid = db.Column("cl_id_guid",UUID(as_uuid=True),unique=True)
+
+	def to_json_api(self):
+		data = {
+			"cl_id": self.cl_id,
+			"cl_datetime": self.cl_datetime,
+			"cl_total": self.cl_total,
+			"cl_type": self.cl_type,
+			"cl_trans_name": self.cl_trans_name,
+			"inv_id": self.inv_id,
+			"bank_fich_head_id": self.bank_fich_head_id,
+			"arap_id": self.arap_id,
+			"ks_line_id": self.ks_line_id,
+			"cl_credit": self.cl_credit,
+			"arap_id_acc_card_id": self.arap_id_acc_card_id,
+			"op_bl_l_id": self.op_bl_l_id,
+			"inv_real_code": self.inv_real_code,
+			"spe_code": self.spe_code,
+			"group_code": self.group_code,
+			"security_code": self.security_code,
+			"cl_desc": self.cl_desc,
+			"p_id": self.p_id,
+			"rep_rate": self.rep_rate,
+			"rep_debit": self.rep_debit,
+			"rep_credit": self.rep_credit,
+			"cl_id_guid": self.cl_id_guid,
+		}
+
+		return data
---
+++ api/main/models/ak_models/Mat_trans_line.py
@@ -0,0 +1,123 @@
+# CREATE TABLE doner_test.dbo.tbl_mg_mat_trans_line (
+# 	mat_trans_line_id int IDENTITY(1,1) NOT NULL,
+# 	mat_trans_line_date datetime DEFAULT getdate() NULL,
+# 	material_id int NULL,
+# 	fich_line_id int NULL,
+# 	mat_inv_line_id int NULL,
+# 	arap_id int NULL,
+# 	mat_trans_type_id int NULL,
+# 	mat_trans_type_code nvarchar(50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+# 	mat_trans_line_amount_out decimal(18,5) NULL,
+# 	mat_trans_line_price_out decimal(18,5) NULL,
+# 	mat_trans_line_totalprice decimal(18,5) NULL,
+# 	mat_trans_line_nettotal decimal(18,5) NULL,
+# 	mat_trans_line_amount_in decimal(18,5) NULL,
+# 	mat_trans_line_price_in decimal(18,5) NULL,
+# 	mat_trans_line_totalprice_in decimal(18,5) NULL,
+# 	mat_trans_line_nettotal_in decimal(18,5) NULL,
+# 	mat_trans_line_wh_id_out int NULL,
+# 	mat_trans_line_wh_id_in int NULL,
+# 	mat_trans_line_wh_amount decimal(18,5) NULL,
+# 	p_id int NULL,
+# 	fich_type_id int NULL,
+# 	mat_inv_type_id int NULL,
+# 	mat_trans_line_cdate datetime DEFAULT getdate() NULL,
+# 	unit_det_id int DEFAULT 0 NULL,
+# 	mat_trans_line_wh_id_amount float DEFAULT 0 NULL,
+# 	inv_real_code nvarchar(50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+# 	fich_line_expiredate datetime DEFAULT '1.1.1900' NULL,
+# 	mat_trans_line_id_out int DEFAULT 0 NULL,
+# 	mat_trans_line_amount_in_onhand decimal(18,5) DEFAULT 0 NULL,
+# 	mat_trans_line_outcost decimal(18,5) DEFAULT 0 NULL,
+# 	mat_trans_line_retcost decimal(18,5) DEFAULT 0 NULL,
+# 	mat_trans_line_out_remain_amount decimal(18,5) DEFAULT 0 NULL,
+# 	mat_trans_line_id_guid nvarchar(50) COLLATE SQL_Latin1_General_CP1_CI_AS DEFAULT newid() NULL,
+# 	rep_rate decimal(18,5) DEFAULT 0 NULL,
+# 	rep_currency_id int DEFAULT 0 NULL,
+# 	CONSTRAINT PK_tbl_mg_mat_trans_line PRIMARY KEY (mat_trans_line_id)
+# );
+
+
+from datetime import datetime
+from sqlalchemy.dialects.postgresql import UUID
+
+from main import db
+
+class Mat_trans_line(db.Model):
+	__tablename__ = "tbl_mg_mat_trans_line"
+	mat_trans_line_id = db.Column("mat_trans_line_id",db.Integer,nullable=False,primary_key=True)
+	mat_trans_line_date = db.Column("mat_trans_line_date",db.DateTime,default=datetime.now())
+	material_id = db.Column("material_id",db.Integer)
+	fich_line_id = db.Column("fich_line_id",db.Integer)
+	mat_inv_line_id = db.Column("mat_inv_line_id",db.Integer)
+	arap_id = db.Column("arap_id",db.Integer)
+	mat_trans_type_id = db.Column("mat_trans_type_id",db.Integer)
+	mat_trans_type_code = db.Column("mat_trans_type_code",db.Integer)
+	mat_trans_line_amount_out = db.Column("mat_trans_line_amount_out",db.Integer)
+	mat_trans_line_price_out = db.Column("mat_trans_line_price_out",db.Integer)
+	mat_trans_line_totalprice = db.Column("mat_trans_line_totalprice",db.Integer)
+	mat_trans_line_nettotal = db.Column("mat_trans_line_nettotal",db.Integer)
+	mat_trans_line_amount_in = db.Column("mat_trans_line_amount_in",db.Integer)
+	mat_trans_line_price_in = db.Column("mat_trans_line_price_in",db.Integer)
+	mat_trans_line_totalprice_in = db.Column("mat_trans_line_totalprice_in",db.Integer)
+	mat_trans_line_nettotal_in = db.Column("mat_trans_line_nettotal_in",db.Integer)
+	mat_trans_line_wh_id_out = db.Column("mat_trans_line_wh_id_out",db.Integer)
+	mat_trans_line_wh_id_in = db.Column("mat_trans_line_wh_id_in",db.Integer)
+	mat_trans_line_wh_amount = db.Column("mat_trans_line_wh_amount",db.Integer)
+	p_id = db.Column("p_id",db.Integer)
+	fich_type_id = db.Column("fich_type_id",db.Integer)
+	mat_inv_type_id = db.Column("mat_inv_type_id",db.Integer)
+	mat_trans_line_cdate = db.Column("mat_trans_line_cdate",db.DateTime,default=datetime.now())
+	unit_det_id = db.Column("unit_det_id",db.Integer)
+	mat_trans_line_wh_id_amount = db.Column("mat_trans_line_wh_id_amount",db.Integer)
+	inv_real_code = db.Column("inv_real_code",db.Integer)
+	fich_line_expiredate = db.Column("fich_line_expiredate",db.DateTime)
+	mat_trans_line_id_out = db.Column("mat_trans_line_id_out",db.Integer)
+	mat_trans_line_amount_in_onhand = db.Column("mat_trans_line_amount_in_onhand",db.Integer)
+	mat_trans_line_outcost = db.Column("mat_trans_line_outcost",db.Integer)
+	mat_trans_line_retcost = db.Column("mat_trans_line_retcost",db.Integer)
+	mat_trans_line_out_remain_amount = db.Column("mat_trans_line_out_remain_amount",db.Integer)
+	mat_trans_line_id_guid = db.Column("mat_trans_line_id_guid",UUID(as_uuid=True),unique=True)
+	rep_rate = db.Column("rep_rate",db.Integer)
+	rep_currency_id = db.Column("rep_currency_id",db.Integer)
+
+	def to_json_api(self):
+		data = {
+			"mat_trans_line_id": self.mat_trans_line_id,
+			"mat_trans_line_date": self.mat_trans_line_date,
+			"material_id": self.material_id,
+			"fich_line_id": self.fich_line_id,
+			"mat_inv_line_id": self.mat_inv_line_id,
+			"arap_id": self.arap_id,
+			"mat_trans_type_id": self.mat_trans_type_id,
+			"mat_trans_type_code": self.mat_trans_type_code,
+			"mat_trans_line_amount_out": self.mat_trans_line_amount_out,
+			"mat_trans_line_price_out": self.mat_trans_line_price_out,
+			"mat_trans_line_totalprice": self.mat_trans_line_totalprice,
+			"mat_trans_line_nettotal": self.mat_trans_line_nettotal,
+			"mat_trans_line_amount_in": self.mat_trans_line_amount_in,
+			"mat_trans_line_price_in": self.mat_trans_line_price_in,
+			"mat_trans_line_totalprice_in": self.mat_trans_line_totalprice_in,
+			"mat_trans_line_nettotal_in": self.mat_trans_line_nettotal_in,
+			"mat_trans_line_wh_id_out": self.mat_trans_line_wh_id_out,
+			"mat_trans_line_wh_id_in": self.mat_trans_line_wh_id_in,
+			"mat_trans_line_wh_amount": self.mat_trans_line_wh_amount,
+			"p_id": self.p_id,
+			"fich_type_id": self.fich_type_id,
+			"mat_inv_type_id": self.mat_inv_type_id,
+			"mat_trans_line_cdate": self.mat_trans_line_cdate,
+			"unit_det_id": self.unit_det_id,
+			"mat_trans_line_wh_id_amount": self.mat_trans_line_wh_id_amount,
+			"inv_real_code": self.inv_real_code,
+			"fich_line_expiredate": self.fich_line_expiredate,
+			"mat_trans_line_id_out": self.mat_trans_line_id_out,
+			"mat_trans_line_amount_in_onhand": self.mat_trans_line_amount_in_onhand,
+			"mat_trans_line_outcost": self.mat_trans_line_outcost,
+			"mat_trans_line_retcost": self.mat_trans_line_retcost,
+			"mat_trans_line_out_remain_amount": self.mat_trans_line_out_remain_amount,
+			"mat_trans_line_id_guid": self.mat_trans_line_id_guid,
+			"rep_rate": self.rep_rate,
+			"rep_currency_id": self.rep_currency_id,
+		}
+
+		return data
